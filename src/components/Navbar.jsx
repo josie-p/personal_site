9a1dcd6fca5fe09c3675f56d36bf5a57aa679d39
Your Name <parker.josie.eden@gmail.com>
2023-03-19 15:40:49 -0500

    changed selection color, some hover effects in the folio, and added subject line to mailto- need to finish media queries

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <li><a href="#home">home</a></li>
         <li><a href="#about">about</a></li>
         <li><a href="#portfolio">portfolio</a></li>
-        <li><a href="mailto:parker.josie.eden@gmail.com" title="Contact Josie!">contact me</a></li>
+        <li><a href="mailto:parker.josie.eden@gmail.com?subject=Connect With Josie" title="Contact Josie!">contact me</a></li>
       </ul>
       </div>
     </div>
